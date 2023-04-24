15640e973e1f9e7ad7f9f571f6825e16cf3562de
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-04-24 01:57:17 -0500

    setup and project config

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="text-3xl font-bold underline dark:bg-black darK:text-white">
            Hello world!
         </h1>
       </div>
